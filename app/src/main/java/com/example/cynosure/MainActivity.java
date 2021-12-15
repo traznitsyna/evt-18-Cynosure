faa3f2e2a8d92b21a67f73e5596f4d97bb907249
Evgen-Raznitsyn <earov59@gmail.com>
2021-12-15 13:05:03 +0500

    Живые обои

--- app/src/main/java/com/example/cynosure/MainActivity.java
+++ app/src/main/java/com/example/cynosure/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.cynosure;
 
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(v -> { //ВЫБОР ОБОЕВ ПРИ НАЖАТИИ НА КНОПКУ
+            Intent intent = new Intent();
+            intent.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
+            startActivity(intent);
+        });
     }
 }
